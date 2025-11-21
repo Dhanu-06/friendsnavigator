77fa135688507f466862c439f48e36b4276caebc
Dhanushree R <u25uv23t029021@gmail.com>
2025-11-21 01:35:58 +0000

    fix the errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,10 @@
 'use client';
 
-import { useAuth } from '@/firebase';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
-import { initiateAnonymousSignIn } from '@/firebase/non-blocking-login';
 
 export default function Home() {
   const router = useRouter();
-  const auth = useAuth();
-
-  useEffect(() => {
-    if (auth) {
-      initiateAnonymousSignIn(auth);
-    }
-  }, [auth]);
 
   useEffect(() => {
     router.push('/login');
