99a36ca9ac76bf8fc448d86ac1bc755abfa18621
Dhanushree R <u25uv23t029021@gmail.com>
2025-11-25 06:50:31 +0000

    Try fixing this error: `Console Error: [2025-11-25T06:47:23.614Z] @fireb

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,14 +3,14 @@
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore, connectFirestoreEmulator, initializeFirestore } from 'firebase/firestore'
+import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore'
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
   const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
-  const firestore = initializeFirestore(app, {});
+  const firestore = getFirestore(app);
 
-  // This check MUST be inside the initialize function and should only be 'true'
+  // This check MUST be inside the initialize function and should only be 'true' for local development
   if (process.env.NEXT_PUBLIC_USE_FIRESTORE_EMULATOR === 'true') {
       // It's safe to call this multiple times. It only connects once.
       connectFirestoreEmulator(firestore, '127.0.0.1', 8080);
